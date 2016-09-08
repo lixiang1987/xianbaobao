8103f3719ad4dff6cdf5ccc139582c34a8016bde
lx <lixiang1987@icloud.com>
2016-09-08 23:10:50 +0800

    item json

--- service/api/item.js
+++ service/api/item.js
@@ -23,45 +23,108 @@ router.get('/latest', function (req, res) {
   res.json(router.get_latest_items());
 })
 
+router.get('/getItem', function (req, res) {
+  res.setHeader('content-type', 'application/json');
+  res.json(router.get_item(req.params.itemId));
+})
+
 // utilities
+
+router.get_item = function(itemId) {
+  if(itemId === "1000"){
+    return {
+        "ItemId":"1000",
+        "PubTime": new Date(),
+        "ItemName": "BoseQC35",
+        "Deposit": "500",
+        "Price": "24.99",
+        "Description": "如何用数码设备换来片刻的宁静,就有了下面这篇Bose QC35降噪蓝牙耳机体验。 外观绝不出彩,但可能是佩戴最舒适的耳机之一Bose QC35.",
+        "Thumbnail": "img/portfolio/thumbnails/1.jpg",
+        "OwnerId": "1000",
+        "Reviews": 15,
+        "Rate": 5,
+        "status": "OnShelf"
+    };
+  }
+  else{
+    return {
+        "ItemId":"1001",
+        "PubTime": new Date(),
+        "ItemName": "Second Product",
+        "Deposit": "500",
+        "Price": "64.99",
+        "Description": "This is a short description.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "Thumbnail": "img/portfolio/thumbnails/2.jpg",
+        "OwnerId": "1000",
+        "Reviews": 12,
+        "Rate": 4,
+        "status": "OnShelf"
+    };
+  }
+}
 
 router.get_latest_items = function () {
   return [
     {
+      "ItemId":"1000",
+      "PubTime": new Date(),
       "ItemName": "BoseQC35",
-      "Price": "$24.99",
+      "Deposit": "500",
+      "Price": "24.99",
       "Description": "如何用数码设备换来片刻的宁静,就有了下面这篇Bose QC35降噪蓝牙耳机体验。 外观绝不出彩,但可能是佩戴最舒适的耳机之一Bose QC35.",
       "Thumbnail": "img/portfolio/thumbnails/1.jpg",
+      "OwnerId": "1000",
       "Reviews": 15,
-      "Rate": 5
+      "Rate": 5,
+      "status": "OnShelf"
     }, {
+      "ItemId":"1001",
+      "PubTime": new Date(),
       "ItemName": "Second Product",
-      "Price": "$64.99",
+      "Deposit": "500",
+      "Price": "64.99",
       "Description": "This is a short description.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       "Thumbnail": "img/portfolio/thumbnails/2.jpg",
+      "OwnerId": "1000",
       "Reviews": 12,
-      "Rate": 4
+      "Rate": 4,
+      "status": "OnShelf"
     }, {
+      "ItemId":"1002",
+      "PubTime": new Date(),
       "ItemName": "Third Product",
-      "Price": "$74.99",
+      "Deposit": "500",
+      "Price": "74.99",
       "Description": "This is a short description.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       "Thumbnail": "img/portfolio/thumbnails/3.jpg",
+      "OwnerId": "1000",
       "Reviews": 31,
-      "Rate": 4
+      "Rate": 4,
+      "status": "OnShelf"
     }, {
+      "ItemId":"1003",
+      "PubTime": new Date(),
       "ItemName": "Fourth Product",
-      "Price": "$84.99",
+      "Deposit": "500",
+      "Price": "84.99",
       "Description": "This is a short description.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       "Thumbnail": "img/portfolio/thumbnails/4.jpg",
+      "OwnerId": "1000",
       "Reviews": 6,
-      "Rate": 3
+      "Rate": 3,
+      "status": "OnShelf"
     }, {
+      "ItemId":"1004",
+      "PubTime": new Date(),
       "ItemName": "Fifth Product",
-      "Price": "$94.99",
+      "Deposit": "500",
+      "Price": "94.99",
       "Description": "This is a short description.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       "Thumbnail": "img/portfolio/thumbnails/5.jpg",
+      "OwnerId": "1000",
       "Reviews": 18,
-      "Rate": 4
+      "Rate": 4,
+      "status": "OnShelf"
     }];
 }
 
--- webapps/apps/item.js
+++ webapps/apps/item.js
@@ -1,10 +1,43 @@
-angular.module('app', ['ngRoute'])
+angular.module('itemApp', ['ngRoute'])
   .config(function ($routeProvider) {
     $routeProvider.otherwise({ redirectTo: '/' });
   })
-  .controller('ItemController', function ($scope, ItemService) {
+  .controller('ItemController', function ($scope, $location, ItemService) {
+
+    var self = this;
+    var itemId = null; // optional declaration!
+    $scope.item = null;
+    bind("itemId");
+
+    function bind(valueName) {
+
+        // Controller to URL
+      $scope.$watch(function() { return self[valueName] }, function (newVal) {
+          console.log("Property changed!");
+          $location.search(valueName, newVal);
+      });
+
+      // URL to controller
+      $scope.$on('$locationChangeSuccess', function(event) {
+          console.log("URL changed!");
+          self[valueName] = $location.search()[valueName];
+          ItemService.get(self["itemId"], function (data) {
+          $scope.item = data;
+        });
+      });
+    }
+    
     $scope.submit = function () {
       ItemService.create($scope.description, $scope.rent);
+    }
+  })
+  .factory('ItemService', function ($http) {
+    return {
+      get: function (itemId, callback) {
+        $http.get('/api/item/getItem', {params: { itemId: itemId }}).success(function (data) {
+          callback(data);
+        });
+      }
     }
   })
   .service('ItemService', function ($http) {
@@ -27,5 +60,9 @@ angular.module('app', ['ngRoute'])
 
       });
     };
+    this.get = function (itemId, callback) {
+        $http.get('/api/item/getItem', {params: { itemId: itemId }}).success(function (data) {
+          callback(data);
+        });
+      } 
   });
-
